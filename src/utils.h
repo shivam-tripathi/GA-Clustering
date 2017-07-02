e6ca3d53ddc76220234abbb785a2997b3ecec46e
Shivam Tripathi <shivam.flash@gmail.com>
2017-07-02 14:25:03 +0530

    Add structures and includes in utils file

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,24 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+#include <iostream>
 #include <string>
+#include <vector>
+
+struct instance {
+	float slength;
+	float swidth;
+	float plength;
+	float pwidth;
+	int type;
+	instance(float a, float b, float c, float d, int e)
+	: slength(a), swidth(b), plength(c), pwidth(d), type(e)
+	{}
+};
+
+struct node {
+	int l;
+	int r;
+};
 
 #endif // __UTILS_H__
