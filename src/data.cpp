db9405a1dc132ef99b56cfca485c1bbf507b78dd
Shivam Tripathi <shivam.flash@gmail.com>
2017-07-07 09:56:01 +0530

    Add utility data functions

--- src/data.cpp
+++ src/data.cpp
@@ -1,3 +1,28 @@
 #include "data.h"
 
 Data :: Data (std::string s) : LoadData(s) {}
+
+instance Data :: get_mean(std::vector<int> &indices, std::vector<int> &referential_indices) {
+	instance mean(0,0,0,0,0);
+	for(int i=0; i<referential_indices.size(); i++) {
+		int index = referential_indices[i];
+		int actual_index = indices[index];
+		add_instances(mean, instances[actual_index]);
+	}
+	avg_instance(mean, referential_indices.size());
+	return mean;
+}
+
+void Data :: add_instances(instance &mean, const instance &ins) {
+	mean.slength += ins.slength;
+	mean.swidth += ins.swidth;
+	mean.plength += ins.plength;
+	mean.pwidth += ins.pwidth;
+}
+
+void Data :: avg_instance(instance &mean, int size) {
+	mean.slength /= ((float)size);
+	mean.swidth /= ((float)size);
+	mean.plength /= ((float)size);
+	mean.pwidth /=((float)size);
+}
--- src/data.h
+++ src/data.h
@@ -1,6 +1,14 @@
+#ifndef __DATA_H__
+#define __DATA_H__
+
 #include "load_data.h"
 
 class Data : public LoadData {
 public:
 	Data(std::string s);
+	instance get_mean(std::vector<int> &, std::vector<int> &);
+	void add_instances(instance &, const instance &);
+	void avg_instance(instance &, int);
 };
+
+#endif // __DATA_H__
