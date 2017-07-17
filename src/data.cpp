81ae3388f5fd2ca19b96bd30f8a8845c7ec4e66e
Shivam Tripathi <shivam.flash@gmail.com>
2017-07-17 15:48:21 +0530

    Add avg and distance functions

--- src/data.cpp
+++ src/data.cpp
@@ -18,6 +18,11 @@ void Data :: add_instances(instance &mean, const instance &ins) {
 	mean.swidth += ins.swidth;
 	mean.plength += ins.plength;
 	mean.pwidth += ins.pwidth;
+	mean.type += ins.type;
+}
+
+float Data :: add_instance(const instance &instance) {
+	return (instance.slength + instance.swidth + instance.plength + instance.pwidth);
 }
 
 void Data :: avg_instance(instance &mean, int size) {
@@ -25,4 +30,15 @@ void Data :: avg_instance(instance &mean, int size) {
 	mean.swidth /= ((float)size);
 	mean.plength /= ((float)size);
 	mean.pwidth /=((float)size);
+	mean.type /= ((int)size);
+}
+
+float Data :: find_distance(instance &a, instance &b) {
+	float ans = 0;
+	ans += (a.slength - b.slength) * (a.slength - b.slength);
+	ans += (a.swidth - b.swidth) * (a.swidth - b.swidth);
+	ans += (a.plength - b.plength) * (a.plength - b.plength);
+	ans += (a.pwidth - b.pwidth) * (a.pwidth - b.pwidth);
+	ans += (a.type - b.type) * (a.type - b.type);
+	return ans;
 }
--- src/data.h
+++ src/data.h
@@ -7,8 +7,11 @@ class Data : public LoadData {
 public:
 	Data(std::string s);
 	instance get_mean(std::vector<int> &, std::vector<int> &);
+
 	void add_instances(instance &, const instance &);
+	float add_instance(const instance &);
 	void avg_instance(instance &, int);
+	float find_distance(instance &, instance &);
 };
 
 #endif // __DATA_H__
