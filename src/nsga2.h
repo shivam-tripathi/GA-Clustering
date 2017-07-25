f81b78229d4313eb5bbcf39146aff81235dd42b1
Shivam Tripathi <shivam.flash@gmail.com>
2017-07-25 19:33:17 +0530

    Add Sorting struct and compare function for Sorting

--- src/nsga2.h
+++ src/nsga2.h
@@ -3,10 +3,26 @@
 
 #include "population.h"
 
+struct Sorting {
+	int domination_count;
+	int index;
+	Sorting(int dominated_count, int index) :
+	domination_count(dominated_count), index(index)
+	{}
+};
+
+struct CmpSorting {
+	bool operator() (const Sorting &a, const	Sorting &b) {
+		if (a.domination_count != b.domination_count)
+			return a.domination_count > b.domination_count;
+		return a.index < b.index;
+	}
+};
+
 class NSGA2 {
 public:
 	Population *population;
-	std::vector<Solution> sorted_fronts;
+	int solution_count;
 
 	NSGA2(Population *);
 	void sort();
