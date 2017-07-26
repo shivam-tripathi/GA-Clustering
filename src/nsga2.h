cde3772e7d3b42953e2cdf80f764789ce4067d30
Shivam Tripathi <shivam.flash@gmail.com>
2017-07-27 04:17:38 +0530

    Add various structures and their compare functions, add modularity functions to nsga2

--- src/nsga2.h
+++ src/nsga2.h
@@ -5,16 +5,41 @@
 
 struct Sorting {
 	int domination_count;
+	float distance;
 	int index;
-	Sorting(int dominated_count, int index) :
-	domination_count(dominated_count), index(index)
+	Sorting(int index, int dominated_count, float distance) :
+	index(index), domination_count(dominated_count), distance(distance)
 	{}
 };
 
 struct CmpSorting {
-	bool operator() (const Sorting &a, const	Sorting &b) {
+	bool operator() (const Sorting &a, const Sorting &b) {
 		if (a.domination_count != b.domination_count)
-			return a.domination_count > b.domination_count;
+			return a.domination_count < b.domination_count;
+		if (a.distance != b.distance)
+			return a.distance > b.distance;
+		return a.index < b.index;
+	}
+};
+
+struct Objective {
+	int index;
+	float value;
+	Objective(int i, int k) :
+	index(i), value(k)
+	{}
+};
+
+struct CmpObjective {
+	bool operator() (const Objective &a, const Objective &b) {
+		if (a.value != b.value)
+			return a.value > b.value;
+		return a.index < b.index;
+	}
+};
+
+struct CmpErase {
+	bool operator() (const Sorting &a, const Sorting &b) {
 		return a.index < b.index;
 	}
 };
@@ -25,7 +50,9 @@ public:
 	int solution_count;
 
 	NSGA2(Population *);
-	void sort();
+	void sort(int);
+	void set_distance();
+	void set_domination_count();
 };
 
 #endif // __NSGA_2__
