cd184c5c91b1bb7004be40bc138854fdc8fa718e
Shivam Tripathi <shivam.flash@gmail.com>
2017-07-26 22:17:14 +0530

    Add print population function and fix regression in the chromosomes in the population

--- src/population.cpp
+++ src/population.cpp
@@ -2,9 +2,21 @@
 
 Population :: Population(Data &data, int n) {
 	this->data = &(data);
+	solutions.clear();
 	for(int i=0; i<n; i++) {
-		Chromosome chromo(data);
-		solutions.push_back(Solution(&chromo));
+		Chromosome *chromo = new Chromosome(data);
+		solutions.push_back(Solution(chromo, i));
+		// printf("\t\t%lu ->\t%f %d ==> %d \n", solutions.size(), solutions[i].chromosome->mean, solutions[i].chromosome->clusters_count, solutions[i].index);
+		// print_population();
 		printf("Chromosome %d populated \n", i+1);
+		printf("\n\n");
+	}
+}
+
+void Population :: print_population() {
+	for (int i=0; i<solutions.size(); i++) {
+		printf("Printing population --> \t\t\t%f %d ==> %d \n", solutions[i].chromosome->mean, solutions[i].chromosome->clusters_count, solutions[i].index);
+		// print_chromosome((solutions[i].chromosome));
+		// printf("\n");
 	}
 }
