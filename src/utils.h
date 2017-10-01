44e1fe965d377c0c9352a7f4af078287a4436f1c
Shivam Tripathi <shivam.flash@gmail.com>
2017-10-02 03:16:21 +0530

    utils.h: Add infinite variable

--- src/utils.h
+++ src/utils.h
@@ -9,10 +9,10 @@
 #include <utility>
 #include <vector>
 
+const int inf = (int)1e9+100;
 static std::vector<int> DEFAULT_INT_VECTOR;
 static std::vector<float> DEFAULT_FLOAT_VECTOR;
 static std::random_device rd;
-const int inf = (int)1e9+100;
 
 struct instance {
 	float slength;
