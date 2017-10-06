51bd7b86747acac3415c50c61b7fe5d0e110e69e
Shivam Tripathi <shivam.flash@gmail.com>
2017-10-06 07:28:03 +0530

    main.cpp: Add epochs

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,14 @@ int main(int argc, char const *argv[]) {
 	count_chromosomes = 10;
 
 	// Pretty printing
-	for(int i=0; i<50; i++) printf("*");
+	for(int i=0; i<100; i++) printf("*");
 	std::cout << "\n\nFile input: " << name << std::endl;
 	std::cout << "Number of chromosomes: " << count_chromosomes << "\n" << std::endl;
-	for(int i=0; i<50; i++) printf("*");
+	for(int i=0; i<100; i++) printf("*");
 	printf("\n");
 
 
 	Data data(name);
-	// Population *population = new Population(data, count_chromosomes);
 	Population population(data, count_chromosomes);
 
 	// Write tests to verify if all data has loaded successfully
@@ -28,11 +27,34 @@ int main(int argc, char const *argv[]) {
 
 	// Beginning execution
 	printf("\n");
-	for(int i=0; i<50; i++) printf("*");
+	for(int i=0; i<100; i++) printf("*");
 
  	printf("\n\nBeginning execution ... ");
 
  	NSGA2 solve(population);
- 	solve.sort();
+
+	// Run the GA for 100 turns
+	int epoch = 100, mutation_const = uniform_int(1, 100);
+	while(epoch--) {
+		// Pretty printing
+		for(int i=0; i<100; i++) printf("#");
+		printf("\n\t\t\t\t\t\t\t\t\t\t\tEpoch %d\n", epoch+1);
+		for(int i=0; i<100; i++) printf("#");
+		printf("\n");
+
+		// First sort the population
+		solve.sort();
+
+		// Carry out crossover
+		// During the beginning epochs greater lesser number of elites are withheld
+		// Later, we keep greater number of elite back
+		// Elites withheld are never more than 5
+		int elite_withheld = std::min(5, 10-epoch/10);
+		population.crossover(elite_withheld);
+
+		if (uniform_int(1, 100) == mutation_const) {
+			population.mutation();
+		}
+	}
 	return 0;
 }
